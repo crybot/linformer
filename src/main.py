8cb32e6d7eb0e09ca4fd0d9e64770807d13f7c6a
Marco <marco.pampaloni1@gmail.com>
2024-07-16 12:16:59 +0200

    Add project scaffold

---
+++ src/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import numpy as np
+import pandas as pd
+import math
+import torch
+from torch import nn
+from utils import set_random_state
+
+
+def main():
+    SEED = 42
+    torch.backends.cuda.matmul.allow_tf32 = True
+    set_random_state(SEED)
+
+
+    # Get cpu or gpu device for training.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
+    ########## TODO ############
+
+    ############################
+
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import torch
+import random
+import numpy as np
+
+#TODO: refactor
+#TODO: move to appropriate modules
+#TODO: type annotations
+
+def set_random_state(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
+
+def params_count(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def print_summary(model, print_model=False):
+    if print_model:
+        print(model)
+        print(f'Number of parameters: {params_count(model)}')
