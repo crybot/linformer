0a3a3a5a87084dd80e2066a54e5e85f2797dcad0
Marco <marco.pampaloni1@gmail.com>
2024-09-19 18:19:48 +0200

    Add more default imports to pythonrc

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,3 +1,4 @@
+import importlib
 import torch
 from torch import Tensor, nn
 import numpy as np
@@ -5,6 +6,16 @@ import pandas as pd
 import readline
 import os
 import atexit
+from models.transformers import Transformer, MultiHeadAttention
+from models.transformers import TransformerEncoder, TransformerEncoderLayer
+from models.transformers import TransformerDecoder, TransformerDecoderLayer
+from models.transformers import NLPTransformer 
+from models.transformers import LanguageModelingHead
+from transformers import AutoTokenizer
+import datasets
+from datasets import CSVDataset
+
+
 print("Python interactive shell started with default imports!")
 
 # Point to the history file in the mounted volume
