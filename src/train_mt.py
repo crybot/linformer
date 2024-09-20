a630f071cc3e7124a23af42766eaba5731db9fee
Marco <marco.pampaloni1@gmail.com>
2024-09-20 18:12:16 +0200

    Fix indentation

--- src/train_mt.py
+++ src/train_mt.py
@@ -16,25 +16,33 @@ from evaluation.utils import extract_probs
 from utils import to_device
 
 def multiple_eight(n: int) -> int:
-  return (n + 7) // 8 * 8
+    return (n + 7) // 8 * 8
 
 def max_non_padded_length(sequence: Tensor, pad_token = 1) -> int:
-  non_padded_mask = sequence != pad_token
-  # Sum along the sequence dimension (N) to count non-padding tokens for each sequence
-  lengths = non_padded_mask.sum(dim=1)
+    non_padded_mask = sequence != pad_token
+    # Sum along the sequence dimension (N) to count non-padding tokens for each sequence
+    lengths = non_padded_mask.sum(dim=1)
 
-  # Find the maximum length in the batch
-  return lengths.max().item()
+    # Find the maximum length in the batch
+    return lengths.max().item()
 
 def trim_batch_pad_tokens(inputs: tuple[Tensor]) -> Tensor:
-  src, tgt, src_mask, tgt_mask = inputs
-  max_src_length = max_non_padded_length(src)
-  max_tgt_length = max_non_padded_length(tgt)
+    src, tgt, src_mask, tgt_mask = inputs
+    max_src_length = max_non_padded_length(src)
+    max_tgt_length = max_non_padded_length(tgt)
 
-  src, src_mask = src[:, :max_src_length], src_mask[:, :max_src_length]
-  tgt, tgt_mask = tgt[:, :max_tgt_length], tgt_mask[:, :max_tgt_length]
+    src, src_mask = src[:, :max_src_length], src_mask[:, :max_src_length]
+    tgt, tgt_mask = tgt[:, :max_tgt_length], tgt_mask[:, :max_tgt_length]
 
-  return src, tgt, src_mask, tgt_mask
+    return src, tgt, src_mask, tgt_mask
+
+def seq2seq_perplexity(pred, inputs):
+    _, tgt, _, tgt_mask = inputs
+    tgt_mask = tgt_mask[..., 1:].bool()
+    masked_pred = pred[tgt_mask]
+    masked_tgt = tgt[..., 1:][tgt_mask]
+
+    return perplexity(extract_probs(masked_pred, masked_tgt))
 
 
 class CustomTrainingLoop(TrainingLoop):
@@ -42,38 +50,30 @@ class CustomTrainingLoop(TrainingLoop):
         super().__init__(*args, **kwargs)
 
     def preprocess_batch(
-        self,
-        inputs: tuple[Tensor, Tensor, Tensor, Tensor],
-        *args,
-        **kwargs
-        ) -> tuple[Tensor, Tensor, Tensor, Tensor]:
-      return to_device(*trim_batch_pad_tokens(inputs), device=self.device)
+            self,
+            inputs: tuple[Tensor, Tensor, Tensor, Tensor],
+            *args,
+            **kwargs
+            ) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+        return to_device(*trim_batch_pad_tokens(inputs), device=self.device)
 
     def forward(
-        self,
-        inputs: tuple[Tensor, Tensor, Tensor, Tensor],
-        *args,
-        **kwargs
-        ) -> tuple[Tensor, Tensor]:
+            self,
+            inputs: tuple[Tensor, Tensor, Tensor, Tensor],
+            *args,
+            **kwargs
+            ) -> tuple[Tensor, Tensor]:
 
-      src, tgt, src_mask, tgt_mask = inputs
-      pred = self.model(src, tgt, src_mask, tgt_mask, log=True)
+        src, tgt, src_mask, tgt_mask = inputs
+        pred = self.model(src, tgt, src_mask, tgt_mask, log=True)
 
-      # TODO: refactor masking
-      tgt_mask = tgt_mask[..., 1:].bool()
-      masked_pred = pred[tgt_mask].view(-1, self.model.classes)
-      masked_tgt = tgt[..., 1:][tgt_mask].reshape(-1)
-      loss = self.loss_fn(masked_pred, masked_tgt)
+        # TODO: refactor masking
+        tgt_mask = tgt_mask[..., 1:].bool()
+        masked_pred = pred[tgt_mask].view(-1, self.model.classes)
+        masked_tgt = tgt[..., 1:][tgt_mask].reshape(-1)
+        loss = self.loss_fn(masked_pred, masked_tgt)
 
-      return pred, loss
-
-def seq2seq_perplexity(pred, inputs):
-  _, tgt, _, tgt_mask = inputs
-  tgt_mask = tgt_mask[..., 1:].bool()
-  masked_pred = pred[tgt_mask]
-  masked_tgt = tgt[..., 1:][tgt_mask]
-
-  return perplexity(extract_probs(masked_pred, masked_tgt))
+        return pred, loss
 
 def main():
     dim = 512
